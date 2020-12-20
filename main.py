37ad9c4ef47f77ef06dc3b345d8db6fdeb207e93
KnlnKS <soulkks@hotmail.ca>
2020-12-20 04:07:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,40 +4,43 @@ import statsmodels.api as sm
 
 class CalibratedMultidimensionalCompensator(QCAlgorithm):
 
-class CalibratedMultidimensionalCompensator(QCAlgorithm):
-
     def Initialize(self):
         # Set Stuff
         self.SetCash(100000)
         self.SetStartDate(2017,1,1) 
+        self.SetEndDate(2017,12,1) 
+        self.ticker = "WTICOUSD"
         self.lookback = 4*365 # Lookback window for history request
-        self.lookforwardVal = 10 # How many days into the future to predict data for
+        self.lookforwardVal = 5 # How many days into the future to predict data for
+        self.predVal = 0 # Predicted increase/decrease
         
         self.oil = self.AddCfd("WTICOUSD", Resolution.Daily, Market.Oanda)
-        
-        self.Debug(self.GetPrediction())
-        
+
+    
     def lookforward(self):
         return self.lookback+self.lookforwardVal
    
-    def GetPrediction(self):
+   
+    def getPrediction(self):
         oil_data = self.History(["WTICOUSD"], self.lookback, Resolution.Daily)
         model = sm.tsa.ARIMA(oil_data['close'].values, order=(1, 1, 1)).fit()
         prediction = model.predict(self.lookback, self.lookforward())
-        """
-        
-        """
         return prediction
         
         
-    def bullish():
-        return (self.GetPrediction().sum())>=0
+    def bullish(self):
+        return (self.getPrediction().sum())>=0
+        
+    
+    def increase(self):
+        curr = self.getPrediction().sum()
+        return True
 
 
     def OnData(self, data):
-        """
-        self.forexClose = data["USDCAD"].Ask.Close
-        self.oilClose = data.Bars["WTICOUSD"].Close
-        self.Debug(str(self.forexClose))
-        """
-        exit()
+        if not self.Portfolio["WTICOUSD"].Invested and self.bullish():
+            self.MarketOrder("WTICOUSD", 100)
+        elif self.Portfolio["WTICOUSD"].Invested and not self.bullish():
+            self.MarketOrder("WTICOUSD", -100)
+        
+        self.lookback+=1
