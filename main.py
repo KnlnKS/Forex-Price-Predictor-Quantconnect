7ba283e16d4d837e30d4bb15327a446bfdb282da
KnlnKS <soulkks@hotmail.ca>
2020-12-19 20:46:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import statsmodels as sm
 
 class CalibratedMultidimensionalCompensator(QCAlgorithm):
 
     def Initialize(self):
         # Set Stuff
         #self.SetCash(100000)
-        self.SetStartDate(2020, 5, 1)
-        self.SetEndDate(2020, 5, 2)
+        start_time = datetime(2019, 1, 5) # start datetime for history call
+        end_time = datetime(2019, 12, 3) # end datetime for history call
         
-        # Data Requests
-        self.AddForex("USDCAD", Resolution.Daily, Market.Oanda)
-        forex = self.AddForex("USDCAD", Resolution.Daily, Market.Oanda).Symbol
-        
-        self.AddCfd("WTICOUSD", Resolution.Daily, Market.Oanda)
         oil = self.AddCfd("WTICOUSD", Resolution.Daily, Market.Oanda).Symbol
+        oil_data = self.History(self.Symbol('WTICOUSD'), start_time, end_time)
+        self.Debug(oil_data.head())
+        model = sm.tsa.arima_model.ARIMA(oil_data['Close'].values, order=(1, 1, 5))
+        res = model.fit()
         
-        # Oil History Request
-        self.history = self.History(oil, 1095)
+        start_time = datetime(2019, 12, 4) # start datetime for history call
+        end_time = datetime(2019, 12, 17) # end datetime for history call
+        
+        predction = res.predict(len(oil_data['Close'].values), (len(oil_data['Close'].values)+10))
+        
+        self.Debug(predction)
 
     def OnData(self, data):
         """
@@ -24,3 +29,4 @@ class CalibratedMultidimensionalCompensator(QCAlgorithm):
         self.oilClose = data.Bars["WTICOUSD"].Close
         self.Debug(str(self.forexClose))
         """
+        exit()
