01c73742407ad6915b8ac3859219f1f2e335bf5c
KnlnKS <soulkks@hotmail.ca>
2020-12-22 15:29:16 -0500

    Update arima_evaluator.py

--- arima_evaluator/arima_evaluator.py
+++ arima_evaluator/arima_evaluator.py
@@ -53,4 +53,4 @@ def eval_models(dataset, p_values, d_values, q_values):
 test_length = int(len(oil_data) * 0.8)
 test_length = len(oil_data[meow:])
 print('Testing Predictions for ' + str(test_length) + ' days.')
-eval_models(oil_data, [1], [1], [6,7,8,9])
+eval_models(oil_data, [1], [1], [0,1,2,3,4,5,6,7,8,9])
