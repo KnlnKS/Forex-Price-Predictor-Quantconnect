07039ba88b0e1f5d6165e0d78e5042721eedb7be
KnlnKS <soulkks@hotmail.ca>
2021-02-18 02:35:52 -0500

    Update Algo with Final Version

--- main.py
+++ main.py
@@ -1,68 +1,67 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.api as sm
+
 
 class CalibratedMultidimensionalCompensator(QCAlgorithm):
 
     def Initialize(self):
-        # Set Stuff
+        # Setup
         self.SetCash(100000)
-        self.SetStartDate(2020,1,1) 
-        self.SetEndDate(2020,4,1) 
+        self.SetStartDate(2020, 1, 30)
+        self.SetEndDate(2020, 4, 1)
         self.ticker = "WTICOUSD"
-        self.lookback = 4*365 # Lookback window for history request
-        self.lookforwardVal = 5 # How many days into the future to predict data for
-        self.predVal = 0 # Predicted increase/decrease
-        
+        self.order = (1, 1, 6)  # ARIMA model config values
+        self.lookback = 4 * 365  # Lookback window for history request
+        # self.lookback = 50
+        self.lookforwardVal = 5  # How many days into the future to predict data for
+        self.predVal = 0  # Predicted increase/decrease
         self.oil = self.AddCfd("WTICOUSD", Resolution.Daily, Market.Oanda)
 
-    def lookforward(self):
-        return self.lookback+self.lookforwardVal
-   
-   
-    def getPrediction(self):
-        oil_data = self.History(["WTICOUSD"], self.lookback, Resolution.Daily)
-        model = sm.tsa.ARIMA(oil_data['close'].values, order=(1, 1, 1)).fit()
-        prediction = model.predict(self.lookback, self.lookforward())
-        self.Debug(prediction.sum())
-        return prediction
-        
-    def bullish(self):
-        return (self.getPrediction().sum())>=0
+    def getForecast(self):
+        lfv = self.lookforwardVal
+        oil_data = self.History([self.ticker], self.lookback, Resolution.Daily)
+        model = sm.tsa.ARIMA(oil_data['close'].values, order=self.order).fit()
 
-    
-    def increase(self):
-        curr = self.getPrediction().sum()
-        
-        return True
-        
-        
-    def wow(self, pred, bullish):
-        if bullish:    
-            if pred>0.20:
-                return 0.20
-            elif pred>0.10:
-                return 0.10
+        if lfv == 1:
+            prediction = model.forecast()[0]
         else:
-            if pred<-0.20:
-                return 0.20
-            elif pred<-0.10:
-                return 0.10
-        return 0.05 
+            prediction = model.forecast(lfv)[0]
 
-    
+        return prediction
+
     def OnData(self, data):
-        pred = self.getPrediction().sum()
-        bullish = self.bullish()
-        if -0.01<pred<0.01:
-            pass
-        elif not self.Portfolio["WTICOUSD"].Invested and bullish:
-            self.MarketOrder("WTICOUSD", 2000)
-        elif self.Portfolio["WTICOUSD"].Invested and bullish:
-            multiplier = self.wow(pred, bullish)
-            self.MarketOrder("WTICOUSD", 500)
-        elif self.Portfolio["WTICOUSD"].Invested and not bullish:
-            multiplier = self.wow(pred, bullish)
-            self.MarketOrder("WTICOUSD", -1000)
-        
-        self.lookback+=1
+        currentPrice = data[self.ticker].Price  # Current Oil Price
+        invested = self.Portfolio[self.ticker].Invested  # Oil owned
+        currentCash = self.Portfolio.Cash  # Current cash on hand
+        currentUProfit = self.Portfolio.TotalUnrealizedProfit
+        oilHoldings = self.Portfolio[self.ticker].Quantity  # Amount of oil owned
+        forecast = self.getForecast()
+
+        maximum = max(forecast[0], forecast[1], forecast[2], forecast[3], forecast[4])
+        minimum = min(forecast[0], forecast[1], forecast[2], forecast[3], forecast[4])
+
+        if (maximum - currentPrice) > (currentPrice - minimum):
+
+            if maximum - currentPrice < 0.001 * currentPrice:
+                pass
+            elif 0.001 * currentPrice <= maximum - currentPrice < 0.003 * currentPrice:
+                self.MarketOrder("WTICOUSD", ((0.1 * currentCash) // currentPrice))
+            elif 0.003 * currentPrice <= maximum - currentPrice < 0.005 * currentPrice:
+                self.MarketOrder("WTICOUSD", ((0.2 * currentCash) // currentPrice))
+            else:
+                self.MarketOrder("WTICOUSD", ((0.4 * currentCash) // currentPrice))
+
+        else:
+            if currentPrice - minimum < 0.001 * currentPrice:
+                pass
+            elif 0.001 * currentPrice <= currentPrice - minimum < 0.003 * currentPrice:
+                self.MarketOrder("WTICOUSD", ((-0.1 * currentCash) // currentPrice))
+            elif 0.003 * currentPrice <= currentPrice - minimum < 0.005 * currentPrice:
+                self.MarketOrder("WTICOUSD", ((-0.2 * currentCash) // currentPrice))
+            else:
+                self.MarketOrder("WTICOUSD", ((-0.4 * currentCash) // currentPrice))
+
+        if currentPrice - minimum > (0.01 * currentPrice):
+            self.Liquidate("WTICOUSD")
+
+        self.lookback += 1
